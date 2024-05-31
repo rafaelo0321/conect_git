c94a1524041fea7be1dd1ed3c6b362b38db784bf
rafaelo0321 <rafaninho21@gmail.com>
2024-05-30 21:24:12 -0500

    Add: Inicio del proyecto

---
+++ src/main/java/com/co/conexion/conect/ConectApplication.java
@@ -0,0 +1,13 @@
+package com.co.conexion.conect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectApplication.class, args);
+	}
+
+}
